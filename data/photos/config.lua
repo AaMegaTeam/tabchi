2c5a5e61ed2946f9b74011d8ddd15d4b855040c4
AaMegaTeam <hamed.dehghani321@gmail.com>
2016-11-14 16:30:24 +0330

    Create config.lua

---
+++ data/photos/config.lua
@@ -0,0 +1,15 @@
+do local _ = {
+  enabled_plugins = {
+    "botmanager",
+    "tabchi",
+    "echo"
+  },
+  moderation = {
+    data = "data/moderation.json"
+  },
+  sudo_users = {
+    170172168
+  }
+}
+return _
+end
